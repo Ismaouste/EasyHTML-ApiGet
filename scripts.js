2fc34e4ec9a96e8e11f31bdfc47b7b4831d15e04
Ismael Rodmacq <ismael.rodmacq@gmail.com>
2020-07-15 15:23:12 +0200

    Add .join fonction for the loop to display even better + gitignore updated

--- scripts.js
+++ scripts.js
@@ -5,19 +5,19 @@ function fetchData() {
         })
         .then(foo => {
             const html = foo.map(data => {
-
                 for (const fooKey in foo) {
                     return `<div class="col s12 m6"><div class="card blue-grey darken-1">
                                 <div class="card-content white-text">
                                 <span class="card-title">                                 
                                 <h2>${data.name}</h2></span>
-                                <p class="flow-text"> Age : ${data.age} ans</p>
+                                <p class="flow-text">Age : ${data.age} ans</p>
                                 <p class="flow-text">Marques favorites : ${data.cars}</p> 
                              </div>
                         </div>
                     </div>`;
                 }
             })
+                .join("")
             document.querySelector("#apiData").insertAdjacentHTML("afterbegin", html);
         })
 }
